cf8d139d0ccbeb3275f952b7391e6668f81fb638
widcardw <lui747353367@qq.com>
2025-09-12 11:56:53 +0800

    update `isLatexCode` function

--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,17 @@ const emptyOptions: Readonly<Options> = {
 }
 const emptyClasses: ReadonlyArray<unknown> = []
 
+// This function checks if the given code contains LaTeX code, but it's not AsciiMath embed.
 function isLatexCode(code: string): boolean {
   const latexRegex = /\\([A-Za-z0-9]){2,}/gm
+  const simpleLatexSupSubRegex = /[\^_]\{\s*[a-zA-Z0-9 ]+\s*\}/g
   const texEmbedRegex = /tex".*"/
 
-  return latexRegex.test(code) && !texEmbedRegex.test(code)
+  const hasTrueLatex = latexRegex.test(code)
+  const hasSimpleLatexSupSub = simpleLatexSupSubRegex.test(code)
+  const hasTexEmbed = texEmbedRegex.test(code)
+
+  return (hasTrueLatex || (hasSimpleLatexSupSub && !hasTrueLatex)) && !hasTexEmbed
 }
 
 /**
